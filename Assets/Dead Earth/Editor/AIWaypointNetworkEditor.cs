94e3a844ab18ffc0bc557b20a26a6a2557088ef7
pale-cinder <pale.allusion@gmail.com>
2018-01-31 22:17:18 -0800

    Dead Earth project setup. AI Waypoints Network. NavMesh

---
+++ Assets/Dead Earth/Editor/AIWaypointNetworkEditor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AI;
+
+[CustomEditor(typeof(AIWaypointNetwork))]
+
+public class AIWaypointNetworkEditor : Editor
+{
+
+    private void OnSceneGUI()
+    {
+        AIWaypointNetwork network = (AIWaypointNetwork)target;
+
+        for (int i=0; i<network.Waypoints.Count;i++)
+        {
+            if (network.Waypoints[i]!=null)
+                Handles.Label (network.Waypoints[i].position, "Waypoint " + i.ToString ());
+        }
+
+        if (network.DisplayMode == PathDisplayMode.Connections)
+        {
+            //draw a polyline
+            Vector3[] linePoints = new Vector3[network.Waypoints.Count + 1];
+
+            for (int i = 0; i <= network.Waypoints.Count; i++)
+            {
+                int index = i != network.Waypoints.Count ? i : 0;
+
+                if (network.Waypoints[index] != null)
+                    linePoints[i] = network.Waypoints[index].position;
+
+                else
+                    linePoints[i] = new Vector3(Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
+
+            }
+            Handles.color = Color.green;
+            Handles.DrawPolyLine(linePoints);
+        }
+        else
+        if (network.DisplayMode==PathDisplayMode.Path)
+        {
+            NavMeshPath path = new NavMeshPath();
+
+            Vector3 from = network.Waypoints[network.UIStart].position;
+            Vector3 to = network.Waypoints[network.UIEnd].position;
+
+            NavMesh.CalculatePath(from, to, NavMesh.AllAreas, path);
+            Handles.color=Color.yellow;
+            Handles.DrawPolyLine(path.corners);
+        }
+    }
+}
---
+++ Assets/Dead Earth/Scripts/AIWaypointNetwork.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PathDisplayMode { None, Connections, Path}
+
+public class AIWaypointNetwork : MonoBehaviour
+{
+    public PathDisplayMode DisplayMode=PathDisplayMode.Connections;
+
+    public int UIStart = 0;
+    public int UIEnd = 0;
+
+    public List<Transform> Waypoints = new List<Transform>();
+
+}
