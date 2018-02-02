66487676697ebb61845df56f509b06533f2ddab5
pale-cinder <pale.allusion@gmail.com>
2018-02-01 22:03:22 -0800

    Moving Nav Mesh Agent to the destination

--- Assets/Dead Earth/Scripts/NavAgentExample.cs
+++ Assets/Dead Earth/Scripts/NavAgentExample.cs
@@ -7,6 +7,12 @@ using UnityEngine.AI;
 
 public class NavAgentExample : MonoBehaviour
 {
+    //Inspector Assigned Variable
+    public AIWaypointNetwork WaypointNetwork = null;
+    public int CurrentIndex = 0;
+
+
+    //Private Members 
     private NavMeshAgent _navAgent = null;
 
 
@@ -15,6 +21,17 @@ public class NavAgentExample : MonoBehaviour
     {
 
         _navAgent = GetComponent<NavMeshAgent>();
+
+        if (WaypointNetwork == null) return;
+
+        //If we may got forgot assign the transform
+
+        if (WaypointNetwork.Waypoints[CurrentIndex] != null)
+        {
+            _navAgent.destination = WaypointNetwork.Waypoints[CurrentIndex].position;
+        }
+
+
 
 	}
 	
