7adda99635c95517102226933cdae2969042b18a
pale-cinder <pale.allusion@gmail.com>
2018-02-01 21:50:20 -0800

    Nav Mesh Agent Example script

---
+++ Assets/Dead Earth/Scripts/NavAgentExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class NavAgentExample : MonoBehaviour
+{
+    private NavMeshAgent _navAgent = null;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        _navAgent = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
