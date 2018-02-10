1f7dee1bb187ea5706477520b9c6f9b458d6eab9
pale-cinder <pale.allusion@gmail.com>
2018-02-10 15:14:06 -0800

    Smoother locomotion from Running to Walking

--- Assets/Dead Earth/Scripts/Controller.cs
+++ Assets/Dead Earth/Scripts/Controller.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     private Animator _animator = null;
+    private int _horizontalHash = 0;
+    private int _verticalHash = 0;
+
 
 	// Use this for initialization
 	void Start ()
     {
         _animator = GetComponent<Animator>();
+        _horizontalHash = Animator.StringToHash("Horizontal");
+        _verticalHash = Animator.StringToHash("Vertical");
 
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        //  * 2.32f and * 5.72f max values in the blend tree
         float xAxis = Input.GetAxis("Horizontal") * 2.32f;
         float yAxis = Input.GetAxis("Vertical") * 5.72f;
-
-        _animator.SetFloat("Horizontal", xAxis);
-        _animator.SetFloat("Vertical", yAxis);
+        
+        // --!play with speed for slowing down and going faster
+        _animator.SetFloat(_horizontalHash, xAxis, 0.8f, Time.deltaTime);
+        _animator.SetFloat(_verticalHash, yAxis, 1.0f, Time.deltaTime);
 	}
 }
