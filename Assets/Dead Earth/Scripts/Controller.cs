8c9fce10b80db29ff6dca00a203a20299dad9bb9
pale-cinder <pale.allusion@gmail.com>
2018-02-10 14:53:14 -0800

    Locomotion

---
+++ Assets/Dead Earth/Scripts/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Animator _animator = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float xAxis = Input.GetAxis("Horizontal") * 2.32f;
+        float yAxis = Input.GetAxis("Vertical") * 5.72f;
+
+        _animator.SetFloat("Horizontal", xAxis);
+        _animator.SetFloat("Vertical", yAxis);
+	}
+}
