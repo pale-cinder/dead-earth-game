3ca560b618ba2c6e2ad480a63710ef0055fc4346
pale-cinder <pale.allusion@gmail.com>
2018-02-10 15:47:24 -0800

    Attack with LMB when walk or run animation

--- Assets/Dead Earth/Scripts/Controller.cs
+++ Assets/Dead Earth/Scripts/Controller.cs
@@ -9,12 +9,19 @@ public class Controller : MonoBehaviour
     private int _verticalHash = 0;
 
 
+    //set the value of Attack triger to ON when the fire button is pressed
+    private int _attackHash = 0;
+
+
+
 	// Use this for initialization
 	void Start ()
     {
         _animator = GetComponent<Animator>();
         _horizontalHash = Animator.StringToHash("Horizontal");
         _verticalHash = Animator.StringToHash("Vertical");
+
+        _attackHash = Animator.StringToHash("Attack");
 
 	}
 	
@@ -24,7 +31,10 @@ public class Controller : MonoBehaviour
         //  * 2.32f and * 5.72f max values in the blend tree
         float xAxis = Input.GetAxis("Horizontal") * 2.32f;
         float yAxis = Input.GetAxis("Vertical") * 5.72f;
-        
+
+        //checking if LMB is pressed
+        if (Input.GetMouseButtonDown(0)) _animator.SetTrigger(_attackHash);
+
         // --!play with speed for slowing down and going faster
         _animator.SetFloat(_horizontalHash, xAxis, 0.8f, Time.deltaTime);
         _animator.SetFloat(_verticalHash, yAxis, 1.0f, Time.deltaTime);
