869b7b92b601a8521bb59b5b7425258096ab5ae5
pale-cinder <pale.allusion@gmail.com>
2018-02-09 18:31:58 -0800

    Speed controller

---
+++ Assets/Dead Earth/Scripts/SpeedController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedController : MonoBehaviour
+{
+    public float Speed = 0.0f;
+
+    private Animator _controller = null;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        _controller = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        _controller.SetFloat("Speed", Speed);
+	}
+}
