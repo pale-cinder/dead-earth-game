aac86657a2540d77a936979c19fc9bcfdc978e86
pale-cinder <pale.allusion@gmail.com>
2018-02-09 16:46:45 -0800

    Camera mount

---
+++ Assets/Dead Earth/Scripts/SmoothCameraMount.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraMount : MonoBehaviour
+
+{
+
+    public Transform Mount = null;
+    public float Speed = 5.0f;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        transform.position = Vector3.Lerp(transform.position, Mount.position, Time.deltaTime * Speed);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Mount.rotation, Time.deltaTime * Speed);
+	}
+}
