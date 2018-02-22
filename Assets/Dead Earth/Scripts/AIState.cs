6e3bcfbc5ecb892e9c5b6b650d0dfa46b8125112
pale-cinder <pale.allusion@gmail.com>
2018-02-21 23:05:37 -0800

    AIState

---
+++ Assets/Dead Earth/Scripts/AIState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public abstract class AIState : MonoBehaviour 
+{
+	
+	public void SetStateMachine( AIStateMachine stateMachine ) { _stateMachine = stateMachine; }
+
+	// Default Handlers
+	public virtual void			OnEnterState()			{}
+	public virtual void 		OnExitState()			{}
+	public virtual void 		OnAnimatorUpdated() 	{}
+	public virtual void 		OnAnimatorIKUpdated()	{}
+	public virtual void 		OnTriggerEvent( AITriggerEventType eventType, Collider other ){}
+	public virtual void 		OnDestinationReached ( bool isReached ) {}
+
+	
+	public abstract AIStateType GetStateType();
+	public abstract AIStateType OnUpdate();
+
+	
+	protected AIStateMachine	_stateMachine;
+}
