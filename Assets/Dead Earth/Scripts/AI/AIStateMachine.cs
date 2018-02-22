e96961f75078d49645f149e1ba30878a7d2496ff
pale-cinder <pale.allusion@gmail.com>
2018-02-22 13:44:46 -0800

    AI State Machine targets

--- Assets/Dead Earth/Scripts/AI/AIStateMachine.cs
+++ Assets/Dead Earth/Scripts/AI/AIStateMachine.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 
 public enum AIStateType { None, Idle, Alerted, Patrol, Attack, Feeding, Pursuit, Dead }
-public enum AITargerTyoe { None, Waypoint, Visual_Player, Visual_Light, Visual_Food, Audio }
+public enum AITargerType { None, Waypoint, Visual_Player, Visual_Light, Visual_Food, Audio }
 
 
 // Potential targets to the AI System
@@ -24,6 +24,39 @@ public struct AITarget
 
     //Time the target was ping'd at last
     private float _time;
+
+
+    public AIStateType type { get { return _type; } }
+
+    private Collider { get { return _collider; } }
+
+    private Vector3  { get { return _position; } } 
+
+    private float { get { return _distance; } } 
+
+    private float { get { return _time; } } 
+
+    public void Set (AITargerType t, Collider c, Vector3 p, float d)
+
+    {
+
+    _type = t;
+    _collider = c;
+    _possition = p;
+    _distance = d;
+    _tyme = Time.time;
+
+    }
+
+
+public void Clear()
+{
+    _type = AITargerType.None;
+    _collider = null;
+    _position = Vector3.zero;
+    _time = 0.0f;
+    _distance = Mathf.Infinity;
+}
 }
 
 
@@ -34,11 +67,17 @@ public abstract class AIStateMachine : MonoBehaviour
 
     //Key to the dictionary                   
 
-    //Private
+    public AITarget VisualThread = new AITarget();
+    public AITarget AudioThread = new AITarget();
+        
+    
+        
+    protected Dictionary<AIStateType, AIState>    _states = new Dictionary<AIStateType, AIState > ();
+    protected AITarget _target = new AITarget();
 
-    private Dictionary<AIStateType, AIState>    _states = new Dictionary<AIStateType, AIState > ();
 
     protected virtual void Start ()
+
     {
         AIState[] states = GetComponent<AIState>();
         
