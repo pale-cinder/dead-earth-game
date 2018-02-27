5d017e0a4611f2571ca0ce954a7f03f33b8c9af4
pale-cinder <pale.allusion@gmail.com>
2018-02-26 23:42:02 -0800

    Examing the distance from the zombie to the player

---
+++ Assets/Dead Earth/Scripts/AI/AIZombieState.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AIZombieState : AIState
+{
+    // Identify different threats
+
+    public override void OnTriggerEvent(AITriggerEventType eventType, Collider other)
+    {
+
+        // base.OnTriggerEvent(eventType, other);
+
+        if (_stateMachine == null)
+            return;
+
+        if (eventType != AITriggerEventType.Exit)
+        {
+            AITriggerEventType curType = _stateMachine.VisualThreat;
+
+            // If it's not a player, examing the tag
+
+        if (other.CompareTag ("Player") )
+            {
+                // Calculate the distance from the zombie to the player
+                Vector3 distance = Vector3.Distance(_stateMachine.sensorPosition, other.transform.position);
+                if  curType != AITargetType.Visual_Player ||
+                    curType == AITargetType.Visual_Player&& distance <_stateMachine.VisualThreat.distance))
+                    {
+
+
+                    }
+
+            }
+
+
+        }
+    }
+
+
+}
