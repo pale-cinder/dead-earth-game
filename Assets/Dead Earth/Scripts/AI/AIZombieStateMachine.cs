f4bc58c8dfd759b5bc8197c82b7af17e991cf79f
pale-cinder <pale.allusion@gmail.com>
2018-02-26 23:16:16 -0800

    Refresh the animator with up-to-date values forZombie State Machine

--- Assets/Dead Earth/Scripts/AI/AIZombieStateMachine.cs
+++ Assets/Dead Earth/Scripts/AI/AIZombieStateMachine.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//testing --- temp
+
+
 public class AIZombieStateMachine : AIStateMachine
 {
     // Inspector assigned
@@ -15,4 +16,50 @@ public class AIZombieStateMachine : AIStateMachine
     [SerializeField] [Range(0.0f, 1.0f)] float _intelligence = 0.5f;
     [SerializeField] [Range(0.0f, 1.0f)] float _satisfaction = 1.0f;
 
+    // Set by states and serialize at state machine
+    private int _seeking = 0;
+    private bool _feeding = false;
+    private bool _crawling = false;
+    private int _attackType = 0;
+
+
+    // Hashes
+    private int _speedHash = Animator.StringToHash("Speed");
+    private int _seekingHash = Animator.StringToHash("Seeking");
+    private int _feedingHash = Animator.StringToHash("Feeding");
+    private int _attackHash = Animator.StringToHash("Attack");
+
+
+    // Properties
+    public float fov { get { return _fov; } }
+    public float hearing { get { return _hearing; } }
+    public float sight { get { return _sight; } }
+    public bool crawling { get { return _crawling; } } // directly translated to the parameter, zombie only is crawling if the lower body is damaged
+    public float intelligence { get { return _intelligence; } }
+    public float satisfaction { get { return _satisfaction; } set { _satisfaction = value; } }
+    public float aggression { get { return _aggression; } set { _aggression = value; } }
+    public int health { get { return _health; } set { _health = value; } }
+    public int attackType { get { return _attackType; } set { _attackType = value; } }
+    public bool feeding { get { return _feeding; } set { _feeding = value; } }
+    public int seeking { get { return _seeking; } set { _seeking = value; } }
+    public float speed
+    {
+        get { return _navAgent != null ? _navAgent.speed : 0.0f; }
+        set { if (_navAgent != null) _navAgent.speed = value; }
+    }
+
+
+    //Refresh the animator with values for its parameters
+    protected override void Update()
+    {
+        base.Update();
+
+        if (_animator!=null)
+        {
+            _animator.SetFloat(_speedHash, _navAgent.speed);
+            _animator.SetBool(_feedingHash, _feeding);
+            _animator.SetInteger(_seekingHash, _seeking);
+            _animator.SetInteger(_attackHash, _attackType);
+        }
+    }
 }
