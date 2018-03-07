64bc3fe4fac73fbaf58c60705989549b5e71c50f
pale-cinder <pale.allusion@gmail.com>
2018-03-06 22:04:27 -0800

    Zombie starting state

---
+++ Assets/Dead Earth/Scripts/AI/AIZombieState_Idle1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIZombieState_Idle1 : AIZombieState
+{
+
+    public override AIStateType GetStateType()
+    {
+
+        Debug.Log("State Type being fetched by state machine");
+
+        return AIStateType.Idle;
+
+    }
+
+
+
+
+    public override AIStateType OnUpdate()
+    {
+
+
+
+        return AIStateType.Idle;
+
+    }
+
+}
