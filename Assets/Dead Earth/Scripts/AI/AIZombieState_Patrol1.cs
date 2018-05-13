2852f61b86ff3f1bc67b3f09a674c85e6d57dcdf
pale-cinder <pale.allusion@gmail.com>
2018-05-12 20:03:22 -0700

    serialize transitions

--- Assets/Dead Earth/Scripts/AI/AIZombieState_Patrol1.cs
+++ Assets/Dead Earth/Scripts/AI/AIZombieState_Patrol1.cs
@@ -6,6 +6,10 @@ public class AIZombieState_Patrol1 : AIZombieState
 
 {
     [SerializeField] AIWaypointNetwork aIWaypointNetwork = null;
+    [SerializeField] bool _randomPartol = false; // (zombie is not fallowing the exact same point we put down
+    [SerializeField] int _currentWaypint = 0;
+    [SerializeField] [Range (0.0f, 3.0f)] float _speed = 1.0f; // for smooth transition between states 
+
 
     // Inspector assighn
     public override AIStateType GetStateType()
